dafd47a2be97cb7544342f8027c00874e678a04a
edith.githinji <edithgithinji2020@gmail.com>
2025-04-01 08:23:31 +0200

    added a todict function

--- models.py
+++ models.py
@@ -4,20 +4,46 @@ db = SQLAlchemy()
 
 class Hero(db.Model):
     __tablename__ = 'heroes'
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    super_name = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
     powers = db.relationship('Power', secondary='hero_powers', backref='heroes')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'super_name': self.super_name,
+            'powers': [power.to_dict() for power in self.powers]
+        }
 
 class Power(db.Model):
     __tablename__ = 'powers'
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.String(255))
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
 
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
+    
     id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
-    strength = db.Column(db.String(50))
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+    strength = db.Column(db.String(50), nullable=False)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'hero_id': self.hero_id,
+            'power_id': self.power_id,
+            'strength': self.strength
+        }
