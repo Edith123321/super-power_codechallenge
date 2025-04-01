64ded322b319f5a2a4461c5f4555089c9b4bac7b
edith.githinji <edithgithinji2020@gmail.com>
2025-04-01 17:33:07 +0200

    include db init to iitialize the app.

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from models import db, Hero, Power, HeroPower
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 migrate = Migrate(app, db) 
+db.init_app(app)
 
 @app.route('/heroes', methods=['GET', 'POST'])
 def heroes():
