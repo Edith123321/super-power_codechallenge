61bb4e8dcc0d7f9e46312ffd82c7ff6e3b8ac432
edith.githinji <edithgithinji2020@gmail.com>
2025-04-01 16:51:27 +0200

    get method on the hero_powers model

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_migrate import Migrate   
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+migrate = Migrate(app, db) 
 
 @app.route('/heroes', methods=['GET', 'POST'])
 def heroes():
@@ -62,38 +64,28 @@ def power(id):
         db.session.commit()
         return jsonify({'message': 'Power deleted'})
 
-@app.route('/hero_powers', methods=['POST'])
+@app.route('/hero_powers', methods=['GET', 'POST'])
 def hero_powers():
-    data = request.json
-    hero_power = HeroPower(
-        strength=data['strength'],
-        hero_id=data['hero_id'],
-        power_id=data['power_id']
-    )
+    if request.method == 'GET':
+        hero_powers = HeroPower.query.all()
+        return jsonify([{'id': hp.id, 'hero_id': hp.hero_id, 'power_id': hp.power_id, 'strength': hp.strength} for hp in hero_powers])
+    if request.method == 'POST':
+        data = request.json
+        hero_power = HeroPower(
+            strength=data['strength'],
+            hero_id=data['hero_id'],
+            power_id=data['power_id']
+        )
+        db.session.add(hero_power)
+        db.session.commit()
+        return jsonify({'id': hero_power.id, 'hero_id': hero_power.hero_id, 'power_id': hero_power.power_id, 'strength': hero_power.strength})
 
-
-@app.route('/heroes/<int:id>', methods=['PATCH'])
-def update_hero(id):
-    hero = Hero.query.get(id)
+@app.route('/hero_powers/<int:id>', methods=['PATCH'])
+def update_hero_power(id):
+    hero_power = HeroPower.query.get(id)
     data = request.json
-    if 'name' in data: 
-        hero.name = data['name']
-    if 'super_name' in data:
-        hero.super_name = data['super_name']
-    db.session.commit()
-    return jsonify({'id': hero.id, 'name': hero.name, 'super_name': hero.super_name})
-
-@app.route('/powers/<int:id>', methods=['PATCH'])
-def update_power(id):
-    power = Power.query.get(id)
-    data = request.json
-    if 'name' in data:
-        power.name = data['name']
-    if 'description' in data:
-        power.description = data['description']
-    db.session.commit()
-    return jsonify({'id': power.id, 'name': power.name, 'description': power.description})
-    db.session.add(hero_power)
+    if 'strength' in data:
+        hero_power.strength = data['strength']
     db.session.commit()
     return jsonify({'id': hero_power.id, 'hero_id': hero_power.hero_id, 'power_id': hero_power.power_id, 'strength': hero_power.strength})
 
